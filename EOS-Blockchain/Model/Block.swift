15fb736bcc500f7f7ac6d82c3592ced666cb6f6d
Jay <jay.ambrish.raval@gmail.com>
2019-09-16 15:27:48 -0400

    creating block data model

---
+++ EOS-Blockchain/Model/Block.swift
@@ -0,0 +1,35 @@
+//
+//  Block.swift
+//  EOS-Blockchain
+//
+//  Created by Jay Raval on 9/16/19.
+//  Copyright © 2019 Jay Raval. All rights reserved.
+//
+
+import Foundation
+
+struct Block {
+    let timestamp: String
+    let producer: String
+    let confirmed: Int
+    let previous: String
+    let transactionMRoot: String
+    let actionMRoot: String
+    let scheduleVersion: Int
+    let newProducers: String?
+    let headerExtensions: [String]
+    let producerSignature: String
+    
+    enum CodingKeys: String, CodingKey {
+        case timestamp = "timestamp"
+        case producer = "producer"
+        case confirmed = "confirmed"
+        case previous = "previous"
+        case transactionMRoot = "transaction_mroot"
+        case actionMRoot = "action_mroot"
+        case scheduleVersion = "schedule_version"
+        case newProducers = "new_producers"
+        case headerExtensions = "header_extensions"
+        case producer_signature = "producer_signature"
+    }
+}
